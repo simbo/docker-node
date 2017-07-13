a65ab866df770edce5d99d4d43456476b92eb162
Simon Lepel <sl@bytepark.de>
2017-07-14 01:15:03 +0200

    optimize publish script

--- publish.sh
+++ publish.sh
@@ -4,13 +4,24 @@
 
 set -ex
 
-VERSION="8.1.4"
+VERSION_MAJOR="8"
+VERSION_MINOR="1"
+VERSION_PATCH="4"
+
 IMAGE_NAME="simbo/node"
 
-docker build -t ${IMAGE_NAME}:${VERSION} .
-docker build -f Dockerfile-slim -t ${IMAGE_NAME}:${VERSION}-slim .
-docker build -f Dockerfile-alpine -t ${IMAGE_NAME}:${VERSION}-alpine .
-docker tag ${IMAGE_NAME}:${VERSION} ${IMAGE_NAME}:latest
-docker tag ${IMAGE_NAME}:${VERSION}-slim ${IMAGE_NAME}:latest-slim
-docker tag ${IMAGE_NAME}:${VERSION}-alpine ${IMAGE_NAME}:latest-alpine
+IMAGE_FULL=${IMAGE_NAME}:${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+
+docker build --no-cache --pull -t ${IMAGE_FULL} .
+docker build --no-cache --pull -f Dockerfile-slim -t ${IMAGE_FULL}-slim .
+docker build --no-cache --pull -f Dockerfile-alpine -t ${IMAGE_FULL}-alpine .
+docker tag ${IMAGE_FULL} ${IMAGE_NAME}:latest
+docker tag ${IMAGE_FULL} ${IMAGE_NAME}:${VERSION_MAJOR}.${VERSION_MINOR}
+docker tag ${IMAGE_FULL} ${IMAGE_NAME}:${VERSION_MAJOR}
+docker tag ${IMAGE_FULL}-slim ${IMAGE_NAME}:latest-slim
+docker tag ${IMAGE_FULL}-slim ${IMAGE_NAME}:${VERSION_MAJOR}.${VERSION_MINOR}-slim
+docker tag ${IMAGE_FULL}-slim ${IMAGE_NAME}:${VERSION_MAJOR}-slim
+docker tag ${IMAGE_FULL}-alpine ${IMAGE_NAME}:latest-alpine
+docker tag ${IMAGE_FULL}-alpine ${IMAGE_NAME}:${VERSION_MAJOR}.${VERSION_MINOR}-alpine
+docker tag ${IMAGE_FULL}-alpine ${IMAGE_NAME}:${VERSION_MAJOR}-alpine
 docker push ${IMAGE_NAME}
